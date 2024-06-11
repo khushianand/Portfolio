54b0a8ca38bfa1554dd375de488c9a5417bd45a6
Khushi Anand <khushi.2024it1190@kiet.edu>
2024-06-11 12:49:24 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,6 +7,7 @@ const HeaderSocials = () => {
         <div className='header__socials'>
             <a href='https://www.linkedin.com/in/khushianand091101/' target='_blank' rel='noreferrer'>Linkedln</a>
             <a href='https://github.com/khushianand' target='_blank' rel='noreferrer'>Github</a>
+            <a href='https://www.instagram.com/khush.i091101?igsh=ZHBrY2xoMGtoeGx0' target='_blank' rel='noreferrer'>Insagram</a>
 
         </div>
     )
